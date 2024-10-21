fe06e644fac71a5fbc71bfc6ccd7438eb5915e64
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2024-10-20 20:56:26 -0600

    mostrar información del país

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
 })
 export class CountryPageComponent implements OnInit{
+
+  public country?: Country;
 
   constructor( 
     private activetedRoute: ActivatedRoute,
@@ -28,10 +31,8 @@ export class CountryPageComponent implements OnInit{
           return this.router.navigateByUrl('');
         }
 
-        console.log(`TENEMOS UN PAIS: `);
-        return;
-        
-
+        // console.log(`TENEMOS UN PAIS: `);
+        return this.country = country;       
       })
 
   }
