b0512b406e67d357a30e9347ef1cf38619eb8c23
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2024-10-24 21:05:09 -0600

    Implementación de un debouncer para cuando el usuario deje de escribir

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,10 +1,13 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html'
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit{
+
+  private debouncer = new Subject<string>();
 
   @Input()
   public placeholder: string = '';
@@ -12,8 +15,25 @@ export class SearchBoxComponent {
   @Output()
   public onValue = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+   this.debouncer
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe( value => {
+      this.onDebounce.emit( value );    
+    })
+  }
+
   emitValue( value: string): void {
     this.onValue.emit( value );
+  }
+
+  onKeyPress( searchTerm: string): void {
+    this.debouncer.next( searchTerm ); 
   }
 
 
